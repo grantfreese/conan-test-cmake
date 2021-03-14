47a610d4ad2b31ac091ed317629e1d62f1f13670
Grant Freese <grant@x146.com>
2021-03-14 15:12:03 -0700

    Working x86 build. Arm build needs newlib stubs for _open, etc..

--- conanfile.py
+++ conanfile.py
@@ -4,21 +4,26 @@ from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
 
 class BuildTestConan(ConanFile):
 	name = "conan-test-cmake"
-	version = "0.0.1"
 	settings = "os", "compiler", "build_type", "arch"
 	exports_sources = "*"
-	requires = [
-		"libtest/0.0.2@user/testing",
-		"cmake/3.19.4"]
+	generator = "cmake"
+	requires = ["libtest/0.0.1@aptera/sandbox"]
+	build_requires = ["cmake/3.19.4"]
 
 	def generate(self):
 		tc = CMakeToolchain(self)
+		tc.variables["CMAKE_SYSTEM_NAME"] = "Generic"
+		tc.variables["CMAKE_SYSTEM_PROCESSOR"] = "cortex-m4"
+
+		# disable linking stage of cmake compiler test build (required for crosscompilation)
+		tc.variables["CMAKE_TRY_COMPILE_TARGET_TYPE"] = "STATIC_LIBRARY"
+		#tc.variables[""] = ""
 		tc.generate()
+
 		deps = CMakeDeps(self)
 		deps.generate()
 
 	def build(self):
 		cmake = CMake(self)
-		cmake.configure()
+		cmake.configure(source_folder="src")
 		cmake.build()
-
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
-#include "libtest.h"
+#include "libtest/libtest.h"
 
 int main()
 {
+	int x = 5;
+	while(x > 0)
+	{
+		x--;
+	}
 	std::cout << "test" << std::endl;
 	libtest();
 	return(0);
